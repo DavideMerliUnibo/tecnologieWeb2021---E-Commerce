a4d2c5ab42a9180aa1e249aa9194a31bbd210960
RyanPerrina <ryan.perrina@studio.unibo.it>
2022-02-09 17:30:54 +0100

    aggiunte pagine aboutus e faq e relativi template

---
+++ Website/aboutus.php
@@ -0,0 +1,6 @@
+<?php 
+
+require_once("bootstrap.php");
+$templateParams["nome"] = "about-get.php";
+require("template/base.php");
+?>
---
+++ Website/template/faq-get.php
@@ -0,0 +1,25 @@
+<h1>Le FAQ più comuni:</h1>
+<p>
+  <a class="btn btn-primary" data-bs-toggle="collapse" href="#contatti" role="button" aria-expanded="false" aria-controls="collapseExample">
+    Come contattarci?
+  </a>
+  
+</p>
+<div class="collapse" id="contatti">
+  <div class="card card-body">
+    Chiamaci coglione.
+  </div>
+</div>
+<p>
+    <a class="btn btn-primary" data-bs-toggle="collapse" href="#allergie" role="button" aria-expanded="false" aria-controls="collapseExample">
+         Allergie?
+    </a>
+</p>
+<div class="collapse" id="allergie">
+    <div class="card card-body">
+    Non mangiare funghi.
+    </div>
+
+</div>
+
+  
