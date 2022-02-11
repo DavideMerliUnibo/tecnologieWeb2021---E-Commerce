9f96291302283bb971d8c5431c61870d9be700d4
DavideMerliUnibo <davide.merli5@studio.unibo.it>
2022-02-11 16:14:54 +0100

    Aggiunto titolo pagina a carrello

--- Website/carrello.php
+++ Website/carrello.php
@@ -2,6 +2,7 @@
 
 require_once("bootstrap.php");
 $templateParams["nome"] = "carrello-get.php";
+$templateParams["title"] = "Funghi - Carrello";
 if(isUserloggedIn()){
     $templateParams["prodottiCarrello"] = $dbh-> getProductInCart($_SESSION['email']);
 }else{
