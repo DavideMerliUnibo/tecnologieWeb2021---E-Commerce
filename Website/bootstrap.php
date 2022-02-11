08572fc524d955c45bad06c82a70e0cff41d0a37
RyanPerrina <ryan.perrina@studio.unibo.it>
2022-02-11 18:19:58 +0100

    aggiustamento carrello dalla base.php

--- Website/bootstrap.php
+++ Website/bootstrap.php
@@ -1,6 +1,6 @@
 <?php 
 session_start();
 require_once("db/database.php");
-$dbh = new DatabaseHelper("localhost","root","","websitetecweb");
+$dbh = new DatabaseHelper("localhost","root","","databasefunghi");
 define("UPLOAD_DIR", "upload/");
 require_once("utils/functions.php");
