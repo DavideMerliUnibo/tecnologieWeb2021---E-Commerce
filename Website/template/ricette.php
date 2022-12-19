e96f7f566e91b799559b7a927fe1d00fd96add10
DavideMerliUnibo <davide.merli5@studio.unibo.it>
2022-12-19 16:25:30 +0100

    Modifica visualizzazione immagini

--- Website/template/ricette.php
+++ Website/template/ricette.php
@@ -27,6 +27,8 @@
 
     </div>
 
+</div>
+
     <h2 class="px-5 py-2">Ricette</h2>
 
     <!-- Ricetta -->
@@ -34,16 +36,16 @@
         <?php foreach($templateParams["ricette"] as $ricetta): ?>
         <div class="card my-1 col-12 col-md-3 px-0" >
             <div class="row">
-                <div class="col-6 col-md-12 d-flex">
+                <div class="col-6 col-md-12 mx-auto d-flex">
                     <img src="./img/<?php echo $ricetta["immagine"]; ?>" class="img-fluid rounded-left mx-auto" alt="...">
                 </div>
                 <div class="col-6 col-md-12">
-                <div class="card-body">
-                    <h5 class="card-title"><a href="paginaRicetta.php?titoloRicetta=<?php echo $ricetta["titolo"]; ?>"><?php echo $ricetta["titolo"]; ?></a></h5>
-                    <p class="card-text"><?php echo $ricetta["descrizione"]; ?></p>
-                    <p class="card-text"><small class="text-muted"><?php echo $ricetta["data"]; ?></small></p>
+                    <div class="card-body">
+                        <h5 class="card-title"><a href="paginaRicetta.php?titoloRicetta=<?php echo $ricetta["titolo"]; ?>"><?php echo $ricetta["titolo"]; ?></a></h5>
+                        <p class="card-text"><?php echo $ricetta["descrizione"]; ?></p>
+                        <p class="card-text"><small class="text-muted"><?php echo $ricetta["data"]; ?></small></p>
+                    </div>
                 </div>
-            </div>
             </div>
         </div>
         <?php endforeach; ?>
