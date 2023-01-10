5ee94bef52486428cb10789293bde9908941c1f0
DavideMerliUnibo <davide.merli5@studio.unibo.it>
2023-01-10 16:28:11 +0100

    Modificata visualizzazione ricette

--- Website/template/ricette.php
+++ Website/template/ricette.php
@@ -1,23 +1,61 @@
 <h1 class="px-5 py-2">Ricette</h1>
 
 <!-- Ricetta -->
-<div class="row text-center mx-2">
-    <?php foreach($templateParams["ricette"] as $ricetta): ?>
-    <div class="card my-1 col-12 col-md-3 bg-light">
-        <div class="row">
-            <div class="col-6 col-md-12 mx-auto d-flex">
-                <a href="paginaRicetta.php?titoloRicetta=<?php echo $ricetta["titolo"]; ?>">
-                    <img src="<?php echo UPLOAD_DIR.$ricetta["img"]; ?>" class="img-fluid rounded-left mx-auto" alt="...">
-                </a>
-            </div>
-            <div class="col-6 col-md-12">
-                <div class="card-body">
-                    <h5 class="card-title"><a href="paginaRicetta.php?titoloRicetta=<?php echo $ricetta["titolo"]; ?>"><?php echo $ricetta["titolo"]; ?></a></h5>
-                    <p class="card-text"><?php echo $ricetta["descrizione"]; ?></p>
-                    <p class="card-text"><small class="text-muted"><?php echo $ricetta["data"]; ?></small></p>
+<div class="container py-2">
+    <div class="row text-center g-2">
+        <?php foreach($templateParams["ricette"] as $ricetta): ?>
+        <div class="col-12 col-md-3">
+            <div class="card bg-light my-2">
+                <div class="row">
+                    <div class="col-6 col-md-12 mx-auto d-flex">
+                        <a href="paginaRicetta.php?titoloRicetta=<?php echo $ricetta["titolo"]; ?>">
+                            <img src="<?php echo UPLOAD_DIR.$ricetta["img"]; ?>" class="img-fluid rounded-left mx-auto" alt="...">
+                        </a>
+                    </div>
+                    <div class="col-6 col-md-12">
+                        <div class="card-body">
+                            <h5 class="card-title"><a href="paginaRicetta.php?titoloRicetta=<?php echo $ricetta["titolo"]; ?>"><?php echo $ricetta["titolo"]; ?></a></h5>
+                            <p class="card-text"><?php echo $ricetta["descrizione"]; ?></p>
+                            <p class="card-text"><small class="text-muted"><?php echo $ricetta["data"]; ?></small></p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+        <?php endforeach; ?>
     </div>
-    <?php endforeach; ?>
 </div>
+
+
+ <!--
+<div class="container py-2">
+    <div class="row text-center g-2">
+        <?php foreach ($templateParams["prodotti"] as $prodotto) : ?>
+        <div class="col-12 col-md-3">
+            <div class="card bg-light my-2">
+                <div class="row">
+                    <div class="col-6 col-md-12 mx-auto d-flex">
+                        <a href="product.php?prodotto=<?php echo $prodotto["codice"]; ?>">
+                            <img src="<?php echo UPLOAD_DIR . $prodotto["img"] ?>" class="img-fluid rounded-left mx-auto" alt="...">
+                        </a>
+                    </div>
+                    <div class="col-6 col-md-12">
+                        <div class="card-body">
+                            <h5 class="card-title"><a href="product.php?prodotto=<?php echo $prodotto["codice"]; ?>"><?php echo $prodotto["nomeFungo"]; ?></a></h5>
+                            <p>venduto da <strong><?php echo $prodotto["username"]; ?></strong></p>
+                            <p><strong><?php echo $prodotto["prezzoPerUnità"]; ?> €/Kg</strong></p>
+                            <?php if ($prodotto["quantità"] <= 0) : ?>
+                                <p class="text-danger"> Non disponibile </p>
+                            <?php else : ?>
+                                <p><?php echo $prodotto["quantità"]; ?> in stock</p>
+                            <?php endif; ?>
+                            <p class="card-text"><small class="text-muted"><?php echo $prodotto["data"]; ?></small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php endforeach; ?>
+    </div>
+</div>
+-->
