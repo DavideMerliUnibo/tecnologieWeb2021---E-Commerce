5720970c7a7e7414b2e895b718c568f6e4fa242a
DavideMerliUnibo <davide.merli5@studio.unibo.it>
2023-01-13 16:27:18 +0100

    Aggiunta notifica per quando un prodotto in vendita è esaurito

--- Website/carrello.php
+++ Website/carrello.php
@@ -13,12 +13,15 @@ if(isset($_POST["metodoPagamento"]) && isset($_POST["nomeCarta"]) && isset($_POS
     && isset($_POST["scadenzaCarta"]) && isset($_POST["ccvCarta"]) ){
         $result = $dbh->insertAcquisto($_POST["metodoPagamento"],$_POST["nomeCarta"], $_POST["numeroCarta"],$_POST["scadenzaCarta"],$_POST["ccvCarta"]);
         if( $result=="success"){
-            $prodotti = $dbh -> getProdottiInAcquisto($dbh -> getLatestAcquisto());
+            $prodotti = $dbh -> getProdottiInAcquisto($dbh -> getLatestAcquisto()[0]["codice"]);
             foreach ($prodotti as $prodotto){
                 if($prodotto["quantità"] == 1){
                     $dbh -> insertNotifica("E' stato acquistato un " .$prodotto["nomeFungo"] .".", $prodotto["offerente"]);
                 } else {
                     $dbh -> insertNotifica("Sono stati acquistati " .$prodotto["quantità"] ." " .$prodotto["nomeFungo"] .".", $prodotto["offerente"]);
+                }
+                if ($dbh -> getProductById($prodotto["codice"])[0]["quantità"] == 0){
+                    $dbh -> insertNotifica("Il prodotto " .$prodotto["nomeFungo"] ." è esaurito.", $prodotto["offerente"]);
                 }
             }
             $dbh -> svuotaCarrello($_SESSION["email"]);
