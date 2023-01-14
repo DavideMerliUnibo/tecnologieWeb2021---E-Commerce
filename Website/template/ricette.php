510bc6e08e79d431fe2a4770534be6bcc42abac1
ManuelLuzietti <manuel.luzietti@studio.unibo.it>
2023-01-14 19:16:27 +0100

    inserite barre di ricerca

--- Website/template/ricette.php
+++ Website/template/ricette.php
@@ -1,8 +1,11 @@
 <h1 class="px-5 py-2">Ricette</h1>
-
+<div class="d-flex justify-content-center">
+    <label hidden for="search-bar">Search</label>
+    <input id="search-bar" type="search" class="form-control w-50" placeholder="Search" oninput="return filter(this);"/>
+</div>
 <!-- Ricetta -->
 <div class="container py-2">
-    <div class="row text-center g-2">
+    <div class="row text-center g-2" id="searchResult">
         <?php foreach($templateParams["ricette"] as $ricetta): ?>
         <div class="col-12 col-md-3">
             <div class="card bg-light my-2">
@@ -28,3 +31,32 @@
         <?php endforeach; ?>
     </div>
 </div>
+
+
+<script>
+   
+    const original = document.querySelector("div#searchResult").innerHTML
+    let precFiltered = []
+    function filter(bar){
+        let inserted = bar.value
+        if(inserted === ''){
+            document.querySelector("div#searchResult").innerHTML = original
+        }
+        let elems = document.querySelector("div#searchResult").children
+        let filtered = []
+        for(let x of elems){
+            if (x.children[0].children[0].children[1].children[0].children[0].children[0].textContent.toLowerCase().includes(inserted.toLowerCase())){
+                filtered.push(x)
+            }
+        }
+        let content = ''
+        for(let e of filtered){
+            content += e.outerHTML
+        }
+        if(precFiltered !== content){
+            document.querySelector("div#searchResult").innerHTML = content
+            precFiltered = content
+        }
+       
+    }
+</script>
--- Website/template/shop.php
+++ Website/template/shop.php
@@ -1,10 +1,14 @@
 <header>
     <h1 class="px-5">Shop</h1>
 </header>
+<div class="d-flex justify-content-center">
+    <label hidden for="search-bar">Search</label>
+    <input id="search-bar" type="search" class="form-control w-50" placeholder="Search" oninput="return filter(this);"/>
+</div>
 <div class="container py-2">
-    <div class="row text-center g-2">
+    <div class="row text-center g-2" id="searchResult">
         <?php foreach ($templateParams["prodotti"] as $prodotto) : ?>
-        <div class="col-12 col-md-3">
+        <div class="col-12 col-md-3" >
             <div class="card bg-light my-2">
                 <div class="row">
                     <div class="col-6 col-md-12 mx-auto d-flex">
@@ -34,3 +38,31 @@
         <?php endforeach; ?>
     </div>
 </div>
+
+<script>
+   
+    const original = document.querySelector("div#searchResult").innerHTML
+    let precFiltered = []
+    function filter(bar){
+        let inserted = bar.value
+        if(inserted === ''){
+            document.querySelector("div#searchResult").innerHTML = original
+        }
+        let elems = document.querySelector("div#searchResult").children
+        let filtered = []
+        for(let x of elems){
+            if (x.children[0].children[0].children[1].children[0].children[0].children[0].textContent.toLowerCase().includes(inserted.toLowerCase())){
+                filtered.push(x)
+            }
+        }
+        let content = ''
+        for(let e of filtered){
+            content += e.outerHTML
+        }
+        if(precFiltered !== content){
+            document.querySelector("div#searchResult").innerHTML = content
+            precFiltered = content
+        }
+       
+    }
+</script>
