b502afbd40047298cd3e4902c1b87c5bfac8109d
DavideMerliUnibo <davide.merli5@studio.unibo.it>
2023-01-14 09:41:36 +0100

    Modifica pagina "About Us"

--- Website/template/about-get.php
+++ Website/template/about-get.php
@@ -1,50 +1,41 @@
 <h1 class="px-5 py-2">Chi siamo?</h1>
-<div class="container">
-    <div class="row">
-        <div class="col-12">
-            <div class="card m-3 bg-light">
-                <div class="row">
-                    <div class="col-4">
-                        <img src="img/davide.jpg" class="img-fluid rounded-start" />
-                    </div>
-                    <div class="col-8">
-                        <div class=" card-body">
-                            <p>
-                                Mi chiamo Davide Merli e sono un esperto in fisica quantistica e filosofia. A tempo perso creo siti web stupidi e mangio funghi. Io e questo team di disadattati abbiamo deciso di creare codesto sito di compravendita fungina per dare un luogo sicuro a tutti gli amanti di tali organismi. I have a dream . . .
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="card m-3 bg-light">
-                <div class="row">
-                    <div class="col-8">
-                        <div class=" card-body">
-                            <p>
-                                Mi chiamo Ryan Perrina e sono bravo a dare ripetizioni di matematica. Non so come mi sono ritrovato a far parte di questo team, so solo che ormai è andata così.
-                            </p>
-                        </div>
-                    </div>    
-                    <div class="col-4">
-                        <img src="img/ryan.jpg" class="img-fluid rounded-start" />
-                    </div>
-                    
-                </div>
-            </div>
-            <div class="card m-3 bg-light">
-                <div class="row">
-                    <div class="col-4">
-                        <img src="img/manuel.jpg" class="img-fluid rounded-start" />
-                    </div>
-                    <div class="col-8">
-                        <div class=" card-body">
-                            <p>
-                                Mi chiamo Manuel Luzietti e sono uno a cui piace scrivere codice tutto il giorno e bere Redbull. Sono entrato in questo gruppo giusto per avere qualcosa da fare nella vita.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+<div class="row px-2">
+    <div class="col-12 col-md-4 mb-5 text-center">
+        <div class="bg-white rounded shadow-sm py-5 px-4"><img src="img/davide.jpg" alt="Davide Merli" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+            <h5 class="mb-0">Davide Merli</h5><span class="small text-uppercase text-muted">CEO - Fondatore</span>
+            <p><small>"This is where the fun begins..."</small></p>
+            <ul class="social mb-0 list-inline  mt-3">
+                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+                <li class="list-inline-item"><a href="https://www.instagram.com/davi_merli/" class="social-link"><i class="fa fa-instagram"></i></a></li>
+                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+            </ul>
+        </div>
+    </div>
+            
+    <div class="col-12 col-md-4 mb-5 text-center">
+        <div class="bg-white rounded shadow-sm py-5 px-4"><img src="img/ryan.jpg" alt="Davide Merli" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+            <h5 class="mb-0">Ryan Perrina</h5><span class="small text-uppercase text-muted">Stagista</span>
+            <p><small>"Ripetizioni di matematica go brrrrrrrr"</small></p>
+            <ul class="social mb-0 list-inline mt-3">
+                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+                <li class="list-inline-item"><a href="https://www.instagram.com/ryan_perryy/" class="social-link"><i class="fa fa-instagram"></i></a></li>
+                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="col-12 col-md-4 mb-5 text-center">
+        <div class="bg-white rounded shadow-sm py-5 px-4"><img src="img/manuel.jpg" alt="Davide Merli" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+            <h5 class="mb-0">Manuel Luzietti</h5><span class="small text-uppercase text-muted">Full-stack developer</span>
+            <p><small>"Redbull is love, Redbull is life"</small></p>
+            <ul class="social mb-0 list-inline mt-3">
+                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+                <li class="list-inline-item"><a href="https://www.instagram.com/whois__manuel/" class="social-link"><i class="fa fa-instagram"></i></a></li>
+                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+            </ul>
         </div>
     </div>
 </div>
